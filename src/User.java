20abbdf3ea4a39be09a7500f29f96fb409e8c049
Liang-JH <liangjinghuo@aliyun.com>
2019-12-26 12:36:23 +0800

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,11 @@
+/**
+ * @autor: Liang Jing Huo
+ * @date: 2019/12/26 0026-12:35
+ **/
+public class User
+{
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
